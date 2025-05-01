11f65175a01d785bc936378550e05fba161c044b
Alexander Ceballos <alexander.ceballos.alex@gmail.com>
2025-05-01 13:20:05 -0600

    Fix formato arcchivos de prueba procedimientos

--- Archivos de prueba/PrubeasSPShow.sql
+++ Archivos de prueba/PrubeasSPShow.sql
@@ -1,8 +1,8 @@
 BEGIN
-    -- sp_transactions_by_client(2, TO_DATE('2024-06-01', 'YYYY-MM-DD'), TO_DATE('2025-06-02', 'YYYY-MM-DD'));
-    -- sp_notifications_by_client(2);
-    -- sp_products_by_client(1);
-    -- sp_awards_by_client(1);
+    sp_transactions_by_client(2, TO_DATE('2024-06-01', 'YYYY-MM-DD'), TO_DATE('2025-06-02', 'YYYY-MM-DD'));
+    sp_notifications_by_client(2);
+    sp_products_by_client(1);
+    sp_awards_by_client(1);
     sp_comming_money_by_client(1);
 END;
 /
--- Archivos de prueba/PruebasSPGet.sql
+++ Archivos de prueba/PruebasSPGet.sql
@@ -1,16 +1,7 @@
-/*
-INSERT INTO REMESA (id, pais, fecha, id_cliente, id_tipo_remesa, id_tipo_cuenta)
-VALUES (1, 'México', TO_DATE('2025-04-27', 'YYYY-MM-DD'), 1, 1, 1);
-INSERT INTO PRESTAMO (id, monto_prestamo, tasa_interes, meses, contratacion, fecha_vencimiento, id_cliente)
-VALUES (1, 10000.00, 0.15, 12, TO_DATE('2025-01-01', 'YYYY-MM-DD'), TO_DATE('2025-12-31', 'YYYY-MM-DD'), 1);
-
-*/
-
 BEGIN
 
     sp_comming_money(1, TO_DATE('2025-04-27', 'YYYY-MM-DD'), 'Italia', 1, 1);
 
-
 END;
 /
 
@@ -23,23 +14,18 @@ BEGIN
         sp_get_loan(10000, 15, 3, 1);
 END;
 /
--- sp_transaction(IdTipoTransacción, Fecha, OtrosDetalles, idCliente, IdCuenta/idTarjeta, Valor, idCuentaOrigen, idCuentaDestino)
--- Cada uno de las transacciones se pueden hacer desde una cuenta o tarjeta. Tener en cuenta todas las posibles variantes
+
 BEGIN 
         sp_transaction(1, SYSDATE, 'Tipo 1', 1, 1, 300, 1, 1);
 
 END;
 /
 
--- sp_get_insurance(IdTipoSeguro, ValorSeguro, CantidadPagos, MesesAsegurados, idCliente);
 BEGIN 
 
-        sp_get_insurance(1, 10000, 500.00, 12, 24, 1); -- id debe ser 1
+        sp_get_insurance(1, 10000, 500.00, 12, 24, 1);
 END;
 /
-
-
---    sp_get_product_service(IdTipoProducto, Tipo, PagadoCon , Descripcion, Monto);
 
 BEGIN 
 
@@ -63,8 +49,8 @@ FROM all_cons_columns
 WHERE table_name = 'REMESA';
 */
  
--- SELECT * from REMESA;
--- SELECT * from PRESTAMO;
+SELECT * from REMESA;
+SELECT * from PRESTAMO;
 SELECT * from TRANSACCION;
 SELECT * FROM SEGURO;
 SELECT * FROM SERVICIO;
