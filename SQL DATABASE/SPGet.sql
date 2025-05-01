0ebdd86b46fb2eb19165e6f08ef902cd210a2c65
Alexander Ceballos <alexander.ceballos.alex@gmail.com>
2025-05-01 00:22:25 -0600

    Fix: se reordenan los procedimientos para tener un mayor control

--- SQL DATABASE/SPGet.sql
+++ SQL DATABASE/SPGet.sql
@@ -1,101 +1,132 @@
-CREATE SEQUENCE seq_remesa START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_producto_servicio START WITH 1 INCREMENT BY 1;
 
-CREATE OR REPLACE PROCEDURE sp_comming_money (
-    idTipoRemesa IN INTEGER,
-    fechaRemesa IN DATE,
-    paisRemesa IN VARCHAR,
-    idClienteR IN INTEGER,
-    idCuentaR IN INTEGER
-)
-AS
-    atrTempTipoRemesa NUMBER;
-    atrTempCliente NUMBER;
-    atrTempCuentaR NUMBER;
-    atrTempCuenta NUMBER;
-BEGIN
+CREATE OR REPLACE PROCEDURE sp_get_product_service (
 
+    idTipoProducto IN INTEGER, 
+    tipo IN INTEGER, 
+    pagadoCon IN INTEGER, 
+    descripcion IN VARCHAR, 
+    monto IN NUMBER,
+    idCliente IN INTEGER
+    )
+    AS
+        atrTempTipoServicio NUMBER;
+        atrTempServicio NUMBER;
+        atrTempSaldo NUMBER;
+        atrFechaContratacion DATE := SYSDATE;
     BEGIN
-        SELECT id INTO atrTempTipoRemesa
-        FROM TIPOREMESA
-        WHERE id = idTipoRemesa;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE_APPLICATION_ERROR(-20001, 'No se ha encontrado coincidencias para el tipo de remesa.');
+
+        BEGIN
+            SELECT id INTO atrTempTipoServicio
+            FROM TIPOSERVICIO
+            WHERE id = idTipoProducto;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                RAISE_APPLICATION_ERROR(-20001, 'El tipo de servicio no es valido.');
+        END;
+
+
+        INSERT INTO SERVICIO (id, nombre, monto, id_tipo_servicio)
+        VALUES (seq_producto_servicio.NEXTVAL, descripcion, monto, atrTempTipoServicio)
+        RETURNING id INTO atrTempServicio;
+
+
+        IF tipo = 1 AND monto IS NULL THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Falta el monto para el servicio o producto.');
+        END IF;
+
+        IF monto < 0 THEN
+            RAISE_APPLICATION_ERROR(-20004, 'Solamente se permiten montos mayores o igual a 0.');
+        END IF;
+
+
+        BEGIN
+            SELECT saldo INTO atrTempSaldo
+            FROM CUENTA
+            WHERE id = pagadoCon;
+
+            IF atrTempSaldo < monto THEN
+                RAISE_APPLICATION_ERROR(-20005, 'El sado no es suficiente para adquirir el producto');
+            END IF;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                RAISE_APPLICATION_ERROR(-20006, 'La cuenta ingresada no existe o no pertenece al cliente.');
+        END;
+
+
+        INSERT INTO PRODUCTO_SERVICIO (id, id_cliente, id_servicio)
+        VALUES (seq_producto_servicio.NEXTVAL, idCliente, atrTempServicio);
+
+
+        UPDATE CUENTA
+        SET saldo = saldo - monto
+        WHERE id = pagadoCon;
+
+        COMMIT;
     END;
-
-
-    BEGIN
-        SELECT id INTO atrTempCliente
-        FROM CLIENTE
-        WHERE id = idClienteR;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE_APPLICATION_ERROR(-20002, 'No se ha encontrado coincidencias para el cliente introducido.');
-    END;
-
-    BEGIN
-        SELECT id INTO atrTempCuentaR
-        FROM CUENTA
-        WHERE id = idCuentaR AND id_cliente = idClienteR;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE_APPLICATION_ERROR(-20003, 'Existe un error con la cuenta introducida, no pertenece al cliente o no existe.');
-    END;
-
-    SELECT id_tipo_cuenta
-    INTO atrTempCuenta
-    FROM CUENTA
-    WHERE id = idCuentaR;
-
-    INSERT INTO REMESA (id, pais, fecha, id_cliente, id_tipo_remesa, id_tipo_cuenta)
-    VALUES (seq_remesa.NEXTVAL, paisRemesa, fechaRemesa, idClienteR, idTipoRemesa, atrTempCuenta);
-
-    COMMIT;
-END;
 /
 
-CREATE SEQUENCE seq_prestamo START WITH 1 INCREMENT BY 1;
 
-CREATE OR REPLACE PROCEDURE sp_get_loan (
-    montoPrestamo IN NUMBER,
-    tasaInteres IN INTEGER,
-    meses IN INTEGER,
+CREATE SEQUENCE seq_seguro START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE PROCEDURE sp_get_insurance (
+    idTipoSeguro IN INTEGER,
+    montoAsegurado IN DECIMAL,
+    valorSeguro IN DECIMAL,
+    cantidadPagos IN INTEGER,
+    mesesAsegurado IN INTEGER,
     idClienteR IN INTEGER
-)
-AS
-    atrTempCliente NUMBER;
-    atrFechaContratacion DATE := SYSDATE;
-    atrFechaVencimiento DATE;
-BEGIN
+    )   
+    AS
+        atrTempTipoSeguro NUMBER;
+        atrTempCliente NUMBER;
+        atrFechaContratacion DATE := SYSDATE;
+        atrFechaVencimiento DATE;
+        BEGIN
 
-    BEGIN
-        SELECT id INTO atrTempCliente
-        FROM CLIENTE
-        WHERE id = idClienteR;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE_APPLICATION_ERROR(-20001, 'No se ha encontrado coincidencias para el cliente introducido.');
-    END;
+            BEGIN
+                SELECT id INTO atrTempTipoSeguro
+                FROM TIPOSEGURO
+                WHERE id = idTipoSeguro;
+            EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                    RAISE_APPLICATION_ERROR(-20001, 'El tipo de seguro ingresado no existe.');
+            END;
 
-    IF montoPrestamo < 0 THEN
-        RAISE_APPLICATION_ERROR(-20002, 'Para que el prestamo sea valido el monto debe de ser mayor o igual a 0.');
-    END IF;
+            BEGIN
+                SELECT id INTO atrTempCliente
+                FROM CLIENTE
+                WHERE id = idClienteR;
+            EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                    RAISE_APPLICATION_ERROR(-20002, 'El cliente no existe.');
+            END;
 
-    IF tasaInteres < 0 THEN
-        RAISE_APPLICATION_ERROR(-20003, 'Para que el prestamo sea valido el monto debe de la tasa de interés debe ser mayor o igual a 0.');
-    END IF;
 
-    IF meses < 0 THEN
-        RAISE_APPLICATION_ERROR(-20004, 'El numero de meses debe ser mayor o igual a 0.');
-    END IF;
+            IF montoAsegurado < 0 THEN
+                RAISE_APPLICATION_ERROR(-20003, 'El monto del seguro debe ser mayor o igual a 0.');
+            END IF;
 
-    atrFechaVencimiento := ADD_MONTHS(atrFechaContratacion, meses);
 
-    INSERT INTO PRESTAMO (id, monto_prestamo, tasa_interes, meses, contratacion, fecha_vencimiento, id_cliente)
-    VALUES (seq_prestamo.NEXTVAL, montoPrestamo, tasaInteres, meses, atrFechaContratacion, atrFechaVencimiento, idClienteR);
+            IF valorSeguro < 0 THEN
+                RAISE_APPLICATION_ERROR(-20004, 'El valor del seguro debe ser mayor o igual a 0.');
+            END IF;
 
-    COMMIT;
-END;
+            IF cantidadPagos < 0 THEN
+                RAISE_APPLICATION_ERROR(-20005, 'La cantidad de pagos debe ser mayor o igual a 0.');
+            END IF;
+
+            IF mesesAsegurado < 0 THEN
+                RAISE_APPLICATION_ERROR(-20006, 'Los meses deben de ser mayor o iguales a 0.');
+            END IF;
+
+            atrFechaVencimiento := ADD_MONTHS(atrFechaContratacion, mesesAsegurado);
+
+            INSERT INTO SEGURO (id, monto_asegurado, valor_seguro, cantidad_pagos, meses_asegurado, contratacion, fecha_vencimiento, id_tipo_seguro, id_cliente)
+            VALUES (seq_seguro.NEXTVAL, montoAsegurado, valorSeguro, cantidadPagos, mesesAsegurado, atrFechaContratacion, atrFechaVencimiento, idTipoSeguro, idClienteR);
+
+            COMMIT;
+        END;
 /
 
 CREATE SEQUENCE seq_transaccion START WITH 1 INCREMENT BY 1;
@@ -197,133 +228,102 @@ CREATE OR REPLACE PROCEDURE sp_transaction (
         END;
 /
 
+CREATE SEQUENCE seq_prestamo START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE seq_seguro START WITH 1 INCREMENT BY 1;
-
-CREATE OR REPLACE PROCEDURE sp_get_insurance (
-    idTipoSeguro IN INTEGER,
-    montoAsegurado IN DECIMAL,
-    valorSeguro IN DECIMAL,
-    cantidadPagos IN INTEGER,
-    mesesAsegurado IN INTEGER,
+CREATE OR REPLACE PROCEDURE sp_get_loan (
+    montoPrestamo IN NUMBER,
+    tasaInteres IN INTEGER,
+    meses IN INTEGER,
     idClienteR IN INTEGER
-    )   
-    AS
-        atrTempTipoSeguro NUMBER;
-        atrTempCliente NUMBER;
-        atrFechaContratacion DATE := SYSDATE;
-        atrFechaVencimiento DATE;
-        BEGIN
+)
+AS
+    atrTempCliente NUMBER;
+    atrFechaContratacion DATE := SYSDATE;
+    atrFechaVencimiento DATE;
+BEGIN
 
-            BEGIN
-                SELECT id INTO atrTempTipoSeguro
-                FROM TIPOSEGURO
-                WHERE id = idTipoSeguro;
-            EXCEPTION
-                WHEN NO_DATA_FOUND THEN
-                    RAISE_APPLICATION_ERROR(-20001, 'El tipo de seguro ingresado no existe.');
-            END;
+    BEGIN
+        SELECT id INTO atrTempCliente
+        FROM CLIENTE
+        WHERE id = idClienteR;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20001, 'No se ha encontrado coincidencias para el cliente introducido.');
+    END;
 
-            BEGIN
-                SELECT id INTO atrTempCliente
-                FROM CLIENTE
-                WHERE id = idClienteR;
-            EXCEPTION
-                WHEN NO_DATA_FOUND THEN
-                    RAISE_APPLICATION_ERROR(-20002, 'El cliente no existe.');
-            END;
+    IF montoPrestamo < 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Para que el prestamo sea valido el monto debe de ser mayor o igual a 0.');
+    END IF;
 
+    IF tasaInteres < 0 THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Para que el prestamo sea valido el monto debe de la tasa de interés debe ser mayor o igual a 0.');
+    END IF;
 
-            IF montoAsegurado < 0 THEN
-                RAISE_APPLICATION_ERROR(-20003, 'El monto del seguro debe ser mayor o igual a 0.');
-            END IF;
+    IF meses < 0 THEN
+        RAISE_APPLICATION_ERROR(-20004, 'El numero de meses debe ser mayor o igual a 0.');
+    END IF;
 
+    atrFechaVencimiento := ADD_MONTHS(atrFechaContratacion, meses);
 
-            IF valorSeguro < 0 THEN
-                RAISE_APPLICATION_ERROR(-20004, 'El valor del seguro debe ser mayor o igual a 0.');
-            END IF;
+    INSERT INTO PRESTAMO (id, monto_prestamo, tasa_interes, meses, contratacion, fecha_vencimiento, id_cliente)
+    VALUES (seq_prestamo.NEXTVAL, montoPrestamo, tasaInteres, meses, atrFechaContratacion, atrFechaVencimiento, idClienteR);
 
-            IF cantidadPagos < 0 THEN
-                RAISE_APPLICATION_ERROR(-20005, 'La cantidad de pagos debe ser mayor o igual a 0.');
-            END IF;
-
-            IF mesesAsegurado < 0 THEN
-                RAISE_APPLICATION_ERROR(-20006, 'Los meses deben de ser mayor o iguales a 0.');
-            END IF;
-
-            atrFechaVencimiento := ADD_MONTHS(atrFechaContratacion, mesesAsegurado);
-
-            INSERT INTO SEGURO (id, monto_asegurado, valor_seguro, cantidad_pagos, meses_asegurado, contratacion, fecha_vencimiento, id_tipo_seguro, id_cliente)
-            VALUES (seq_seguro.NEXTVAL, montoAsegurado, valorSeguro, cantidadPagos, mesesAsegurado, atrFechaContratacion, atrFechaVencimiento, idTipoSeguro, idClienteR);
-
-            COMMIT;
-        END;
+    COMMIT;
+END;
 /
 
-CREATE SEQUENCE seq_producto_servicio START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_remesa START WITH 1 INCREMENT BY 1;
 
-CREATE OR REPLACE PROCEDURE sp_get_product_service (
+CREATE OR REPLACE PROCEDURE sp_comming_money (
+    idTipoRemesa IN INTEGER,
+    fechaRemesa IN DATE,
+    paisRemesa IN VARCHAR,
+    idClienteR IN INTEGER,
+    idCuentaR IN INTEGER
+)
+AS
+    atrTempTipoRemesa NUMBER;
+    atrTempCliente NUMBER;
+    atrTempCuentaR NUMBER;
+    atrTempCuenta NUMBER;
+BEGIN
 
-    idTipoProducto IN INTEGER, 
-    tipo IN INTEGER, 
-    pagadoCon IN INTEGER, 
-    descripcion IN VARCHAR, 
-    monto IN NUMBER,
-    idCliente IN INTEGER
-    )
-    AS
-        atrTempTipoServicio NUMBER;
-        atrTempServicio NUMBER;
-        atrTempSaldo NUMBER;
-        atrFechaContratacion DATE := SYSDATE;
     BEGIN
-
-        BEGIN
-            SELECT id INTO atrTempTipoServicio
-            FROM TIPOSERVICIO
-            WHERE id = idTipoProducto;
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                RAISE_APPLICATION_ERROR(-20001, 'El tipo de servicio no es valido.');
-        END;
-
-
-        INSERT INTO SERVICIO (id, nombre, monto, id_tipo_servicio)
-        VALUES (seq_producto_servicio.NEXTVAL, descripcion, monto, atrTempTipoServicio)
-        RETURNING id INTO atrTempServicio;
-
-
-        IF tipo = 1 AND monto IS NULL THEN
-            RAISE_APPLICATION_ERROR(-20003, 'Falta el monto para el servicio o producto.');
-        END IF;
-
-        IF monto < 0 THEN
-            RAISE_APPLICATION_ERROR(-20004, 'Solamente se permiten montos mayores o igual a 0.');
-        END IF;
-
-
-        BEGIN
-            SELECT saldo INTO atrTempSaldo
-            FROM CUENTA
-            WHERE id = pagadoCon;
-
-            IF atrTempSaldo < monto THEN
-                RAISE_APPLICATION_ERROR(-20005, 'El sado no es suficiente para adquirir el producto');
-            END IF;
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                RAISE_APPLICATION_ERROR(-20006, 'La cuenta ingresada no existe o no pertenece al cliente.');
-        END;
-
-
-        INSERT INTO PRODUCTO_SERVICIO (id, id_cliente, id_servicio)
-        VALUES (seq_producto_servicio.NEXTVAL, idCliente, atrTempServicio);
-
-
-        UPDATE CUENTA
-        SET saldo = saldo - monto
-        WHERE id = pagadoCon;
-
-        COMMIT;
+        SELECT id INTO atrTempTipoRemesa
+        FROM TIPOREMESA
+        WHERE id = idTipoRemesa;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20001, 'No se ha encontrado coincidencias para el tipo de remesa.');
     END;
+
+
+    BEGIN
+        SELECT id INTO atrTempCliente
+        FROM CLIENTE
+        WHERE id = idClienteR;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20002, 'No se ha encontrado coincidencias para el cliente introducido.');
+    END;
+
+    BEGIN
+        SELECT id INTO atrTempCuentaR
+        FROM CUENTA
+        WHERE id = idCuentaR AND id_cliente = idClienteR;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Existe un error con la cuenta introducida, no pertenece al cliente o no existe.');
+    END;
+
+    SELECT id_tipo_cuenta
+    INTO atrTempCuenta
+    FROM CUENTA
+    WHERE id = idCuentaR;
+
+    INSERT INTO REMESA (id, pais, fecha, id_cliente, id_tipo_remesa, id_tipo_cuenta)
+    VALUES (seq_remesa.NEXTVAL, paisRemesa, fechaRemesa, idClienteR, idTipoRemesa, atrTempCuenta);
+
+    COMMIT;
+END;
 /
