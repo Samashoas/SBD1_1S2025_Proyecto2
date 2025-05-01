5eef6f722136cf9e819e46e1ec398b6b09d40b8a
FJVicVel03 <3021894750101@ingenieria.usac.edu.gt>
2025-05-01 14:02:57 -0600

    FEATURE vw_active_insurances
    se le tuvo que cambiar el nombre a vw_active_insurances_v2 por problemas

---
+++ Pruebas Fernando/Vistas_Visualizacion.sql
@@ -0,0 +1,41 @@
+SET SERVEROUTPUT ON;
+
+-- 1. Vista de seguros activos
+BEGIN
+   EXECUTE IMMEDIATE 'DROP VIEW vw_active_insurances_v2';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN  -- Si el error no es "vista no existe"
+         RAISE;
+      END IF;
+END;
+/
+
+CREATE OR REPLACE VIEW vw_active_insurances_v2 AS
+SELECT 
+    ts.nombre AS tipo_seguro,
+    -- Información del seguro
+    s.monto_asegurado,
+    s.valor_seguro,
+    s.cantidad_pagos,
+    s.meses_asegurado,
+    s.contratacion,
+    s.fecha_vencimiento,
+    -- Información del cliente
+    c.nombre AS nombre_cliente,
+    c.apellido AS apellido_cliente,
+    ic.telefono,
+    ic.correo
+FROM 
+    SEGURO s
+    JOIN TIPOSEGURO ts ON s.id_tipo_seguro = ts.id
+    JOIN CLIENTE c ON s.id_cliente = c.id
+    JOIN INFOCLIENTE ic ON c.id_info_cliente = ic.id
+WHERE 
+    s.fecha_vencimiento > SYSDATE  -- Solo seguros activos
+ORDER BY 
+    s.fecha_vencimiento  -- Ordenar por fecha de vencimiento más cercana primero
+/
+
+-- Para verificar que la vista se creó correctamente
+SELECT * FROM vw_active_insurances_v2;
